1121680c8a089d1a3ef2f80e58081ccf34a8228d
Patrick Maloney <maloneypatr@gmail.com>
2014-02-20 19:52:22 -0500

    commit 2

---
+++ R/dfToXL.R
@@ -0,0 +1,20 @@
+dfToXL <- function(df, variables, outputFile){
+  require(plyr)
+  require(XLConnect)
+  
+  list <- dlply(df, variables)
+  
+  wb <- loadWorkbook(outputFile, create = T)
+  
+  for(i in 1:length(list))
+    assign(names(list)[i], list[[i]])
+  
+  for(object in names(list)){
+    createSheet(wb, name = object)
+    writeWorksheet(wb, get(object), sheet = object)
+  }
+  
+  saveWorkbook(wb)
+  message('All objects saved to file ', outputFile)
+  
+}
