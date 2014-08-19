1ffc9e720ea66bef1a7430990ab816d4f49f25d4
Patrick Maloney <maloneypatr@gmail.com>
2014-08-19 16:58:29 -0400

    edited hijack.R

--- R/hijack.R
+++ R/hijack.R
@@ -2,7 +2,7 @@ hijack <- function (FUN, ...) {
   .FUN <- FUN
   args <- list(...)
   invisible(lapply(seq_along(args), function(i) {
-    formals(.FUN)[[names(args)[i]]] &lt;<- args[[i]]
+    formals(.FUN)[[names(args)[i]]] <- args[[i]]
   }))
   .FUN
 }
